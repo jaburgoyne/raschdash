05ada0b247ffc283b688105e001248c67a89181c
John Ashley Burgoyne <j.a.burgoyne@uva.nl>
2020-08-06 19:09:25 +0200

    Finally compiling by pretending Stan doesn't exist :(

--- R/raschdash-package.R
+++ R/raschdash-package.R
@@ -1,6 +1,12 @@
 #' @keywords internal
 "_PACKAGE"
 
+# This block is recommended by rstantools
+#' @import Rcpp
+#' @import methods
+#' @importFrom rstan sampling
+# @useDynLib raschdash
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
--- R/rdfit.R
+++ R/rdfit.R
@@ -1,5 +1,3 @@
-#' @importFrom purrr is_character
-#' @importFram tibble has_name
 new_rdfit <- function(data,
                       cohort_colname,
                       group_colname, person_colname,
@@ -7,7 +5,7 @@ new_rdfit <- function(data,
                       max_score_colname, obs_score_colname) {
         data <- tibble::as_tibble(data)
         stopifnot(
-                has_name(
+                tibble::has_name(
                         data,
                         c(
                                 cohort_colname,
@@ -28,3 +26,4 @@ new_rdfit <- function(data,
                       item_colname      = 'Item',
                       max_score_colname = 'Max',
                       obs_score_colname = 'Score')  {
+}
